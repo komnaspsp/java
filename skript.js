482c5160976facd8118b97f21a21b8b2ac307037
nastyaa <anastasiiakomolova20@yandex.ru>
2023-12-02 17:55:16 +0300

    машинки

--- skript.js
+++ skript.js
@@ -1,101 +1,32 @@
-// function sun(a,b){
-//    let c = a + b;
-//    console. log(c);
-//    return c;
-// }
-// const f = sum;
-// f (5,6);
-// const d = (c,u)=>{
-//   let res = c*u;
-//   console. log(` результат умножения S(res)`);
-
-// }
-// d(5,20)
-// let c = "глобальная";
-//  function sun(a,b){
-//    let c = a + b;
-//    console. log(с);
-//    if(c > 12){
-
-
-//     let c = "Область блока";
-//    }
-
-//    console. log(`Выход из блока значение $(c)`);
-//    return c;
-//  }
-
-
-//  sum (5,7);
-//  console. log(c);
-
-// let polkan = {
-// poroda : 'алабай',
-// speak: function() {
-//   console .log(`Gav Gav`)
-// },
-// age: 100
-// }
-// //console.log(polkan);
-
-// polkan.speak();
-// let ar=[2,7,4,9,12,33];
-// const fit (a) => a > 5 
-// let f = ar.filter(fit);
-// console. log(f);
-	let users =[
-{id:	1,
-name:	"Leanne Graham",
-username:	"Bret",
-email:	"Sincere@april.biz",
-age:25,
-address:{
-street:	"маршала Жукова",
-suite:	"Apt. 556",
-city:	"Gwenborough",
-zipcode:	"92998-3874"}
-},
-
-{id:	2,
-name:	"Ervin Howell",
-username:	"Antonette",
-email:	"Shanna@melissa.tv",
-age:25,
-address:{
-street:	"Пер. дунаевского",
-suite:	"Suite 879",
-city:	"Wisokyburgh",
-zipcode:	"90566-7771"}
-},
-
-{
-  id:	3,
-name:	"Clementine Bauch",
-username:	"Samantha",
-lng:	"81.1496",
-phone:	"1-770-736-8031 x56442",
-age:12,
-website:	"hildegard.org",
-address:{
-  suite:	"Suite 879",
-  city:	"Wisokyburgh",
-  zipcode:	"90566-7771"}
+const btleft =document.getElementById("left");
+const btup =document.getElementById("up");
+const btdown =document.getElementById("down");
+const btright =document.getElementById("right");
+let monitor = document.getElementById("net");
+const folder="tanks/";
+let arrPath =["logan","Samoaval","t34","t90","uaz"];
+let curTrans = 0;
+function runleft(){
+console.log("Поехали влево");
+let path = `${folder}${arrPath[curTrans]}/left.jpg`
+monitor.setAttribute("src",path);
 }
-
-//найти пользователей 25 лет и вывести;
-let res1= users.filter(a=> a.age == 25);
-console.table(res1);
-
-//из массива пользователей сделать массив почтовых адресов;
-let res2 = users.map(a=> a.email);
-console.table(res2);
-//найти пользователей, у которых в username нет буквы а;
-let res3 = users.filter(n =>{
-if(n.username.indexOf('a')==-1 && n.username.indexOf('A')==-1){
-  return true;
+function runup(){
+  console.log("Поехали вперед");
+  let path = `${folder}${arrPath[curTrans]}/up.jpg`
+  monitor.setAttribute("src",path);
 }
-return false;
+function rundown(){
+  console.log("Поехали назад");
+  let path = `${folder}${arrPath[curTrans]}/down.jpg`
+  monitor.setAttribute("src",path);
 }
-);
-console.table(res3);
-;
+function runright(){
+  console.log("Поехали вправо");
+  let path = `${folder}${arrPath[curTrans]}/right.jpg`
+  monitor.setAttribute("src",path)
+}
+btleft.addEventListener('click',runleft);
+btup.addEventListener('click',runup);
+btdown.addEventListener('click',rundown);
+btright.addEventListener('click',runright);
