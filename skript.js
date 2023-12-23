f0ce062e48305a8588b5b8d08be5ffb2c4ed1970
nastyaa <anastasiiakomolova20@yandex.ru>
2023-12-23 17:42:50 +0300

    новое

---
+++ skript.js
@@ -0,0 +1,82 @@
+// function test(){
+
+//     console.log(arguments);
+
+// }
+// test(5,7,"ogo");
+
+// let dogs =["Шарик","Бобик","Полкан"];
+
+// [sarik, bobek]= dogs;
+// console.log(sarik, bobek);
+// [bobek,sarik]=[sarik,bobek];
+// console.log(sarik, bobek);
+
+// let car={
+//     weel:"19d",
+//     acselerator: "100",
+//     color: "red"
+// }
+// let{color:r,weel}= car;
+// console.log(r,weel)
+
+// let limonad={
+//     sirop: "Груша",
+//     woter: "0.5",
+//     conservant:"фенил"
+// }
+
+
+// let svetLimonad={
+//     sirop: "Груша",
+//     woter: "0.5",
+//     conservant:"No"
+// }
+// let a=5;
+// let b;
+// b=a;
+// console.log(a=$(a),b=$(b));
+// b=1;
+// console.log(a=$(a),b=$(b));
+
+
+// Limonad= svetLimonad;
+// console.table(Limonad);
+// Limonad.conservant ="Фенил";
+// console.log(newLimonad);
+// console.table(Limonad);
+// console.log("Советский лимонад");
+// console.table(svetLimonad);
+
+
+// Limonad= Object.assign(svetLimonad);
+// console.log(newLimonad);
+// console.table(Limonad);
+// console.log("Советский лимонад");
+// console.table(svetLimonad);
+
+
+// const Dog= function(age,Dname,weight,poroda){
+//     this.age= age;
+//     this.Dname = Dname;
+//     this.weight =weight;
+//     this.poroda=poroda;
+// }
+
+// let sharik= new Dog(2, "Шарик", 5, "Двортерьер");
+// let bobik= new Dog(3,"Бобек", 7, "Дворняга" );
+// console.table(sharik,bobik);
+
+// sharik.speek();
+// bob
+сlass Staff{
+    #userName;
+    #age
+    constructor(uname, age){
+        
+    }
+}
+
+
+
+
